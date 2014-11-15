dd17fa8e909ac3fcaa6515387e60bcd535130156
ayo1103 <ayo1103@gmail.com>
2014-11-15 19:19:56 +0800

    Update server host & port

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,7 +7,10 @@ object Main extends App {
   implicit val system = ActorSystem("test")
   val log = Logging(system, getClass)
 
+  val SERVER_HOST = "0.0.0.0"
+  val SERVER_PORT = Option(System.getenv("PORT")).getOrElse("8080").toInt
+
   log.info("Starting service actor and HTTP server...")
   val service = system.actorOf(Props(new MyServiceActor()), "test-service")
-  IO(Http) ! Http.Bind(service, interface = "0.0.0.0", port = 3000)
+  IO(Http) ! Http.Bind(service, interface = SERVER_HOST, port = SERVER_PORT)
 }
